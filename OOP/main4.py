f697eab9b360f86731886fe65f4dfc583344e689
AayushGuragain <wpd9699@wmich.edu>
2022-02-19 16:52:15 -0500

    variables not in init

--- OOP/main4.py
+++ OOP/main4.py
@@ -18,3 +18,16 @@ item1 = Item('Phone', 100, 5)
 # item1.quantity = 5 #line18
 
 print(item1.name, item1.price, item1.quantity)
+item2 = Item('Car', 20000, 1)
+'''
+So think about our item2 that is car. A car will always have a price
+and we always want to know how many i.e. quantity.
+But what if our item2 is a unique car that can go absoloutely driverless.
+At present context, 2022, it would be a unique car.
+And what if we want to have a boolean to store that data.
+It wont be ideal to change __init__ for that weirdly specific thing.
+So, what can be done? The following:
+'''
+item2.absoloutely_driverless = True
+print(item2.absoloutely_driverless)
+#print(item1.absoloutely_driverless) #should bre error
